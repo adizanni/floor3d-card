b520f3b18df31d4b11e64e2ffc9bbdc692b4bfca
adizanni <adizanni@msn.com>
2021-04-28 22:12:03 +0000

    0.1.3

--- src/floor3d-card.ts
+++ src/floor3d-card.ts
@@ -71,8 +71,8 @@ export class Floor3dCard extends LitElement {
   private _brightness?: number[];
 
   private _firstcall?: boolean;
-  private _card: any;
-  private _box: Element;
+  private _card?: HTMLElement;
+  private _content?: HTMLElement;
 
 
   public static async getConfigElement(): Promise<LovelaceCardEditor> {
@@ -91,6 +91,8 @@ export class Floor3dCard extends LitElement {
   // https://lit-element.polymer-project.org/guide/properties#accessors-custom
   public setConfig(config: Floor3dCardConfig): void {
     // TODO Check for required fields and that they are of the proper format
+    console.log('Set Config');
+
     if (!config) {
       throw new Error(localize('common.invalid_configuration'));
     }
@@ -103,48 +105,61 @@ export class Floor3dCard extends LitElement {
       name: 'Floor 3d',
       ...config,
     };
-    console.log('Start Load');
-    //this._canvasdiv = document.createElement('div');
-    this._firstcall = true;
-    this.display3dmodel();
+
+    if (!this._renderer) {
+
+      console.log('Renderer not instanciated');
+      this.display3dmodel();
+
+    }
+
+
   }
 
   private _firstUpdated(): void {
-    this._box = this.shadowRoot.getElementById('3d_canvas')
-    this._box.appendChild(this._renderer.domElement)
-    this._box.addEventListener("resize", this._resizeCanvas.bind(this));
-    this._box.addEventListener("dblclick", this._showObjectName.bind(this) );
+
+    console.log('First updated start');
+
+    if (!this._content) {
+      console.log('Div not instanciated');
+      this._content = this.shadowRoot.getElementById('3d_canvas');
+      console.log(this._content.id)
+    }
+
+    this._content.appendChild(this._renderer.domElement)
+    window.addEventListener("resize", this._resizeCanvas.bind(this));
+    this._content.addEventListener("dblclick", this._showObjectName.bind(this));
     this._controls = new OrbitControls(this._camera, this._renderer.domElement);
     this._controls.maxPolarAngle = 0.9 * Math.PI / 2;
-    this._controls.addEventListener( 'change', this._render.bind(this) );
-    this._scene.add(new THREE.HemisphereLight( 0xffffbb, 0x080820, 0.5 ));
+    this._controls.addEventListener('change', this._render.bind(this));
+    this._scene.add(new THREE.HemisphereLight(0xffffbb, 0x080820, 0.5));
     this._renderer.render(this._scene, this._camera);
     this._resizeCanvas();
-    this._animate();
+    console.log('First updated end');
   }
 
   private _render(): void {
-    this._resizeCanvas();
     this._renderer.render(this._scene, this._camera);
   }
 
   private _showObjectName(e: any): void {
 
     const mouse: THREE.Vector2 = new THREE.Vector2();
-    mouse.x = ( e.offsetX / this._box.clientWidth ) * 2 - 1;
-    mouse.y = - ( e.offsetY  / this._box.clientHeight ) * 2 + 1;
+    mouse.x = (e.offsetX / this._content.clientWidth) * 2 - 1;
+    mouse.y = - (e.offsetY / this._content.clientHeight) * 2 + 1;
     const raycaster: THREE.Raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera( mouse, this._camera );
-    const intersects: THREE.Intersection[] = raycaster.intersectObjects( this._scene.children,true );
-    if(intersects.length > 0 && intersects[0].object.name != ''){
-      window.prompt("Object:",intersects[0].object.name);
+    raycaster.setFromCamera(mouse, this._camera);
+    const intersects: THREE.Intersection[] = raycaster.intersectObjects(this._scene.children, true);
+    if (intersects.length > 0 && intersects[0].object.name != '') {
+      window.prompt("Object:", intersects[0].object.name);
     }
   }
 
   public set hass(hass: HomeAssistant) {
 
     if (this.config.entities) {
-      if (this._firstcall) {
+      if (!this._states) {
+        console.log('Hass State Change Init')
         this._states = [];
         this._color = [];
         this._brightness = [];
@@ -158,24 +173,25 @@ export class Floor3dCard extends LitElement {
           if (hass.states[entity.entity].attributes['brightness']) {
             this._brightness.push(hass.states[entity.entity].attributes['brightness']);
           } else {
-            this._brightness.push(0);
+            this._brightness.push(-1);
           }
         });
         this._firstcall = false;
       }
       else {
-        this.config.entities.forEach((entity,i) => {
-          if (entity.type3d =='light') {
+        let torerender = false;
+        this.config.entities.forEach((entity, i) => {
+          if (entity.type3d == 'light') {
             let toupdate = false;
-            if ( this._states[i] !== hass.states[entity.entity].state ) {
-              this._states[i] =  hass.states[entity.entity].state;
+            if (this._states[i] !== hass.states[entity.entity].state) {
+              this._states[i] = hass.states[entity.entity].state;
               toupdate = true;
             }
             if (hass.states[entity.entity].attributes['rgb_color']) {
-                if (hass.states[entity.entity].attributes['rgb_color'] !== this._color[i]) {
-                  toupdate = true;
-                  this._color[i] = hass.states[entity.entity].attributes['rgb_color'];
-                }
+              if (hass.states[entity.entity].attributes['rgb_color'] !== this._color[i]) {
+                toupdate = true;
+                this._color[i] = hass.states[entity.entity].attributes['rgb_color'];
+              }
             }
             if (hass.states[entity.entity].attributes['brightness']) {
               if (hass.states[entity.entity].attributes['brightness'] !== this._brightness[i]) {
@@ -184,18 +200,24 @@ export class Floor3dCard extends LitElement {
               }
             }
             if (toupdate) {
-              this._updatelight(entity,this._states[i],this._color[i],this._brightness[i]);
+              this._updatelight(entity, this._states[i], this._color[i], this._brightness[i]);
+              torerender = true;
             }
           }
           else if (this._states[i] !== hass.states[entity.entity].state) {
-            this._states[i] =  hass.states[entity.entity].state;
-            if (entity.type3d =='color') {
-              this._updatecolor(entity,this._states[i]);
-            } else if (entity.type3d =='hide') {
-              this._updatehide(entity,this._states[i]);
+            this._states[i] = hass.states[entity.entity].state;
+            if (entity.type3d == 'color') {
+              this._updatecolor(entity, this._states[i]);
+              torerender = true;
+            } else if (entity.type3d == 'hide') {
+              this._updatehide(entity, this._states[i]);
+              torerender = true;
             }
           }
-        })
+        });
+        if (torerender) {
+          this._render();
+        }
       }
     }
 
@@ -203,6 +225,7 @@ export class Floor3dCard extends LitElement {
 
   protected display3dmodel(): void {
 
+    console.log('Start Build Renderer');
     this._scene = new THREE.Scene();
     if (this.config.backgroundColor && this.config.backgroundColor != '#000000') {
       this._scene.background = new THREE.Color(this.config.backgroundColor);
@@ -236,166 +259,173 @@ export class Floor3dCard extends LitElement {
 
     } else {
       const objLoader: OBJLoader = new OBJLoader();
-      objLoader.load(this.config.path + this.config.objfile, this._onLoaded3DModel.bind(this), this._onLoadObjectProgress.bind(this) , function (error: ErrorEvent): void {
-          throw new Error(error.error);
-        });
+      objLoader.load(this.config.path + this.config.objfile, this._onLoaded3DModel.bind(this), this._onLoadObjectProgress.bind(this), function (error: ErrorEvent): void {
+        throw new Error(error.error);
+      });
     }
 
+    console.log('End Build Renderer');
 
   }
 
-  private _onLoadMaterialProgress (_progress: ProgressEvent): void {
+  private _onLoadMaterialProgress(_progress: ProgressEvent): void {
     return
   }
 
-  private _onLoadObjectProgress (_progress: ProgressEvent): void {
+  private _onLoadObjectProgress(_progress: ProgressEvent): void {
     return
   }
 
-private _resizeCanvas(): void {
+  private _resizeCanvas(): void {
     // Display canvas
+    console.log('Resize canvas start');
     const canvas: Element = this._renderer.domElement;
-    this._camera.aspect = canvas.clientWidth/canvas.clientHeight;
+    this._camera.aspect = canvas.clientWidth / canvas.clientHeight;
     this._camera.updateProjectionMatrix();
-    this._renderer.setSize(canvas.clientWidth,canvas.clientHeight,false);
-    this._renderer.render( this._scene, this._camera );
+    this._renderer.setSize(canvas.clientWidth, canvas.clientHeight, true);
+    this._renderer.render(this._scene, this._camera);
+    console.log('Resize canvas end');
   }
 
-private _onLoaded3DModel(object: THREE.Object3D): void {
-  const bBox: THREE.Box3 = new THREE.Box3().setFromObject(object);
-  this._camera.position.set(bBox.max.x*1.3,bBox.max.y*1.3,bBox.max.z*1.3);
-  this._modelX = object.position.x = -(bBox.max.x - bBox.min.x) / 2;
-  this._modelY = object.position.y = - bBox.min.y;
-  this._modelZ = object.position.z = -(bBox.max.z - bBox.min.z) / 2;
-  this._scene.add(object);
-  this._camera.lookAt(object.position);
-  this._add3dObjects();
-  this._firstUpdated();
-}
+  private _onLoaded3DModel(object: THREE.Object3D): void {
+    console.log('Object loaded start');
+    const bBox: THREE.Box3 = new THREE.Box3().setFromObject(object);
+    this._camera.position.set(bBox.max.x * 1.3, bBox.max.y * 1.3, bBox.max.z * 1.3);
+    this._modelX = object.position.x = -(bBox.max.x - bBox.min.x) / 2;
+    this._modelY = object.position.y = - bBox.min.y;
+    this._modelZ = object.position.z = -(bBox.max.z - bBox.min.z) / 2;
+    this._scene.add(object);
+    this._camera.lookAt(object.position);
+    this._add3dObjects();
+    this._firstUpdated();
+    console.log('Object loaded end');
+  }
 
-private _onLoaded3DMaterials(materials: MTLLoader.MaterialCreator): void {
-  //materials. .lights = false;
-  materials.preload();
-  const objLoader: OBJLoader = new OBJLoader();
-  objLoader.setMaterials(materials);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  objLoader.load(this.config.path+this.config.objfile,this._onLoaded3DModel.bind(this),function(_progress: ProgressEvent) {
-  return }, function(error: ErrorEvent): void {
-    throw new Error(error.error);
-  })
-}
+  private _onLoaded3DMaterials(materials: MTLLoader.MaterialCreator): void {
+    //materials. .lights = false;
+    console.log('Material loaded start');
+    materials.preload();
+    const objLoader: OBJLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load(this.config.path + this.config.objfile, this._onLoaded3DModel.bind(this), function (_progress: ProgressEvent) {
+      return
+    }, function (error: ErrorEvent): void {
+      throw new Error(error.error);
+    });
+    console.log('Material loaded end');
+  }
 
-private _animate(): void {
-  requestAnimationFrame(this._animate.bind(this));
-  this._render();
-  this._controls.update();
-}
+  /*
+  private _animate(): void {
+    requestAnimationFrame(this._animate.bind(this));
+    this._render();
+    this._controls.update();
+  }*/
 
-private _add3dObjects(): void {
+  private _add3dObjects(): void {
 
-  this.config.entities.forEach((entity, i) => {
+    console.log('Add 3D Object start');
+    this.config.entities.forEach((entity, i) => {
 
 
-    const _foundobject: any = this._scene.getObjectByName(entity.object_id)
+      const _foundobject: any = this._scene.getObjectByName(entity.object_id)
 
-    if (!_foundobject) {
-      return;
+      if (_foundobject) {
+        if (entity.type3d == 'light') {
+
+          const box: THREE.Box3 = new THREE.Box3();
+          box.setFromObject(_foundobject);
+          const light: THREE.PointLight = new THREE.PointLight(new THREE.Color('#ffffff'), 0, 300, 2);
+          light.position.set((box.max.x - box.min.x) / 2 + box.min.x + this._modelX, (box.max.y - box.min.y) / 2 + box.min.y + this._modelY, (box.max.z - box.min.z) / 2 + box.min.z + this._modelZ);
+          light.castShadow = true;
+          light.name = entity.light_name;
+          this._scene.add(light);
+          this._updatelight(entity, this._states[i], this._color[i], this._brightness[i]);
+        } else if (entity.type3d == 'color') {
+          _foundobject.material = _foundobject.material.clone();
+          this._updatecolor(entity, this._states[i]);
+        } else if (entity.type3d == 'hide') {
+          this._updatehide(entity, this._states[i]);
+        }
+      }
+    });
+    console.log('Add 3D Object End');
+  }
+
+  private _RGBToHex(r: number, g: number, b: number): string {
+
+    let rs: string = r.toString(16);
+    let gs: string = g.toString(16);
+    let bs: string = b.toString(16);
+
+    if (rs.length == 1)
+      rs = "0" + rs;
+    if (gs.length == 1)
+      gs = "0" + gs;
+    if (bs.length == 1)
+      bs = "0" + bs;
+
+    return "#" + rs + gs + bs;
+  }
+
+  private _updatelight(item: EntityFloor3dCardConfig, state: string, color: number[], brightness: number): void {
+
+    const light: any = this._scene.getObjectByName(item.light_name);
+    if (!light) {
+      return
     }
+    let max: number;
 
-    if (entity.type3d == 'light') {
-
-      const box: THREE.Box3 = new THREE.Box3();
-      box.setFromObject(_foundobject);
-      const light: THREE.PointLight = new THREE.PointLight(new THREE.Color('#ffffff'), 0, 300, 2);
-      light.position.set((box.max.x - box.min.x) / 2 + box.min.x + this._modelX, (box.max.y - box.min.y) / 2 + box.min.y + this._modelY, (box.max.z - box.min.z) / 2 + box.min.z + this._modelZ);
-      light.castShadow = true;
-      light.name = entity.light_name;
-      this._scene.add(light);
-      this._updatelight(entity, this._states[i], this._color[i], this._brightness[i]);
-    } else if (entity.type3d == 'color') {
-        _foundobject.material = _foundobject.material.clone();
-        this._updatecolor(entity, this._states[i]);
-    } else if (entity.type3d == 'hide') {
-      this._updatehide(entity, this._states[i]);
-    }
-  });
-
-}
-
-private _RGBToHex(r: number,g: number,b: number): string {
-
-  let rs: string = r.toString(16);
-  let gs: string = g.toString(16);
-  let bs: string = b.toString(16);
-
-  if (rs.length == 1)
-    rs = "0" + rs;
-  if (gs.length == 1)
-    gs = "0" + gs;
-  if (bs.length == 1)
-    bs = "0" + bs;
-
-  return "#" + rs + gs + bs;
-}
-
-private _updatelight(item: EntityFloor3dCardConfig,state: string,color: number[],brightness: number): void {
-
-  const light: any = this._scene.getObjectByName(item.light_name);
-  if (!light) {
-    return
-  }
-  let max: number;
-
-  if (item.lumens) {
-    max = item.lumens;
-  } else {
-    max = 800;
-  }
-
-  if (state == 'on') {
-    if (brightness) {
-      light.intensity = 0.01 * max * brightness / 255;
+    if (item.lumens) {
+      max = item.lumens;
     } else {
-      light.intensity = 0.01 * max;
+      max = 800;
     }
-    if (!color) {
-      light.color = new THREE.Color('#ffffff');
-    }
-    else {
-      light.color = new THREE.Color(this._RGBToHex(color[0], color[1], color[2]));
-    }
-  } else {
-    light.intensity = 0;
+
+    if (state == 'on') {
+      if (brightness!=-1) {
+        light.intensity = 0.01 * max * brightness / 255;
+      } else {
+        light.intensity = 0.01 * max;
+      }
+      if (color.length==0) {
+        light.color = new THREE.Color('#ffffff');
+      }
+      else {
+        light.color = new THREE.Color(this._RGBToHex(color[0], color[1], color[2]));
+      }
+    } else {
+      light.intensity = 0;
       //light.color = new THREE.Color('#000000');
     }
   }
 
-private _updatecolor(item: EntityFloor3dCardConfig,state: string): void {
+  private _updatecolor(item: EntityFloor3dCardConfig, state: string): void {
 
-  const _object: any = this._scene.getObjectByName(item.object_id);
+    const _object: any = this._scene.getObjectByName(item.object_id);
 
-  let i: any;
+    let i: any;
 
-  for(i in item.conditions) {
-    if (state == item.conditions[i].state ) {
-      _object.material.color.set(item.conditions[i].color);
-      return;
+    for (i in item.conditions) {
+      if (state == item.conditions[i].state) {
+        _object.material.color.set(item.conditions[i].color);
+        return;
+      }
     }
   }
-}
 
 
-private _updatehide(item: EntityFloor3dCardConfig, state: string): void {
+  private _updatehide(item: EntityFloor3dCardConfig, state: string): void {
 
-  const _object: any = this._scene.getObjectByName(item.object_id);
+    const _object: any = this._scene.getObjectByName(item.object_id);
 
-  if ( state == item.state ) {
-    _object.visible = false;
-  } else {
-    _object.visible = true;
+    if (state == item.state) {
+      _object.visible = false;
+    } else {
+      _object.visible = true;
+    }
+
   }
-
-}
 
 
   // https://lit-element.polymer-project.org/guide/lifecycle#shouldupdate
@@ -410,6 +440,7 @@ private _updatehide(item: EntityFloor3dCardConfig, state: string): void {
   // https://lit-element.polymer-project.org/guide/templates
   protected render(): TemplateResult | void {
     // TODO Check for stateObj or other necessary things and render a warning if missing
+    console.log('Render start');
     if (this.config.show_warning) {
       return this._showWarning(localize('common.show_warning'));
     }
@@ -418,18 +449,17 @@ private _updatehide(item: EntityFloor3dCardConfig, state: string): void {
       return this._showError(localize('common.show_error'));
     }
 
+    console.log('Render end');
+
     return html`
-      <ha-card
-        .header=${this.config.name}
-        tabindex="0"
-        .label=${`Floor3d: ${this.config.entity || 'No Entity Defined'}`}
-        .style=${`${this.config.style || 'width: auto; height: 100vh'}`}
-        id="ha-card-1"
-      >
-      <div id="3d_canvas" height="100%"></div>
+      <ha-card .header=${this.config.name} tabindex="0" .label=${`Floor3d: ${this.config.entity || 'No Entity Defined' }`}
+        .style=${`${this.config.style || 'width: auto; height: 100vh' }`} id="ha-card-1">
+        <div id='3d_canvas' style='width: 100%; height: 100%'>
+        </div>
       </ha-card>
     `;
   }
+
 
   /*
   @action=${this._handleAction}
